f3b90689951480e4d7a65c6f8607b5e36bde9d3e
RNAV2022 <ryannav2019@gmail.com>
2023-06-27 21:43:35 +0100

    Mac compatible

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ Uses the mod ID from curseforge to list all the mods in the pack
 Head to https://console.curseforge.com/ and create an account.
 Go to API Keys in the left panel and copy the API Key to the clipboard.
 
-Go to the python file and edit line 9 to use your personal API Key.
+Go to the .env file and enter your personal API Key.
 
 ### Usage
 Go to https://www.curseforge.com/minecraft/modpacks and choose a modpack.
 Copy the Project ID on the right and then run the python file. Enter the Project ID and
-it will then list the mods in that modpack.
+it will then list the mods in that modpack and the aggregate of the mods.
--- curseforge_modpack_lister.py
+++ curseforge_modpack_lister.py
@@ -1,37 +1,41 @@
-import requests, zipfile, io, json, os
+from dotenv import load_dotenv
+import requests
+import zipfile
+import io
+import json
+import os
 
-# Works with windows only
-curr_user = os.getlogin()
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 headers = {
-  'Content-Type': 'application/json',
-  'Accept': 'application/json',
-  'x-api-key': '' # Enter your curseforge api key here
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    # Enter your curseforge api key here
+    'x-api-key': api_key
 }
 
 mods = input('What is the id of the modpack that you would like the list of: ')
-
-modIds = {'modIds': [mods]}
-
 base_url = 'https://api.curseforge.com'
 
-r = requests.post(f'{base_url}/v1/mods', headers=headers, json=modIds)
+if (os.listdir(os.getcwd()).count(mods) == 0):
+    modIds = {'modIds': [mods]}
+    r = requests.post(f'{base_url}/v1/mods', headers=headers, json=modIds)
+    print(r.json()['data'][0]['latestFiles'][0]['downloadUrl'])
+    downloadURL = r.json()['data'][0]['latestFiles'][0]['downloadUrl']
+    r2 = requests.get(downloadURL)
+    z = zipfile.ZipFile(io.BytesIO(r2.content))
+    z.extractall(mods)
+    mod_data = ''
+    mod_name = ''
 
-print(r.json()['data'][0]['latestFiles'][0]['downloadUrl'])
-downloadURL = r.json()['data'][0]['latestFiles'][0]['downloadUrl']
+with open(f'{os.getcwd()}/{mods}/manifest.json', 'r') as f:
+    data = json.load(f)
+    for i in range(len(data['files'])):
+        mod_data = requests.get(
+            f'{base_url}/v1/mods/{data["files"][i]["projectID"]}', headers=headers)
+        mod_name = mod_data.json()['data']['name']
+        print(mod_name)
 
-r2 = requests.get(downloadURL)
-z = zipfile.ZipFile(io.BytesIO(r2.content))
-extracted_path = f'C:\\Users\\{curr_user}\\Desktop\\extracted'
-z.extractall(extracted_path)
-
-mod_data = ''
-mod_name = ''
-
-with open(f'{extracted_path}\\manifest.json', 'r') as f:
-  data = json.load(f)
-  print(f'There are {len(data["files"])} mods in {r.json()["data"][0]["name"]}')
-  for i in range(len(data['files'])):
-    mod_data = requests.get(f'{base_url}/v1/mods/{data["files"][i]["projectID"]}', headers=headers)
-    mod_name = mod_data.json()['data']['name']
-    print(mod_name)
+print(
+    f'There are {len(data["files"])} mods in {data["name"]}')
